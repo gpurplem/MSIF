ed88d2027049d11d3aaa624bb2b96e5ce2c56389
gpurplem <megabrielbm@gmail.com>
2023-05-08 21:45:18 -0300

    Tela lista de contatos e pesquisa usuários

---
+++ frmListaContatos.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MSIF
+{
+    partial class frmListaContatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btnEnviarMsg = new Button();
+            btnDeletarContato = new Button();
+            btnVisualizarPerfil = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(776, 341);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnEnviarMsg
+            // 
+            btnEnviarMsg.Location = new Point(12, 359);
+            btnEnviarMsg.Name = "btnEnviarMsg";
+            btnEnviarMsg.Size = new Size(180, 34);
+            btnEnviarMsg.TabIndex = 1;
+            btnEnviarMsg.Text = "Enviar mensagem";
+            btnEnviarMsg.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletarContato
+            // 
+            btnDeletarContato.Location = new Point(12, 399);
+            btnDeletarContato.Name = "btnDeletarContato";
+            btnDeletarContato.Size = new Size(180, 34);
+            btnDeletarContato.TabIndex = 2;
+            btnDeletarContato.Text = "Deletar contato";
+            btnDeletarContato.UseVisualStyleBackColor = true;
+            // 
+            // btnVisualizarPerfil
+            // 
+            btnVisualizarPerfil.Location = new Point(198, 359);
+            btnVisualizarPerfil.Name = "btnVisualizarPerfil";
+            btnVisualizarPerfil.Size = new Size(169, 34);
+            btnVisualizarPerfil.TabIndex = 3;
+            btnVisualizarPerfil.Text = "Visualizar perfil";
+            btnVisualizarPerfil.UseVisualStyleBackColor = true;
+            // 
+            // frmListaContatos
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnVisualizarPerfil);
+            Controls.Add(btnDeletarContato);
+            Controls.Add(btnEnviarMsg);
+            Controls.Add(dataGridView1);
+            Name = "frmListaContatos";
+            Text = "MSIF - Lista contatos";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnEnviarMsg;
+        private Button btnDeletarContato;
+        private Button btnVisualizarPerfil;
+    }
+}
---
+++ frmPesquisarUsuarios.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MSIF
+{
+    partial class frmPesquisarUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblApelido = new Label();
+            txtApelido = new TextBox();
+            btnPesquisar = new Button();
+            dataGridView1 = new DataGridView();
+            Apelido = new DataGridViewTextBoxColumn();
+            btnEnviarSolicitacao = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblApelido
+            // 
+            lblApelido.AutoSize = true;
+            lblApelido.Location = new Point(19, 33);
+            lblApelido.Name = "lblApelido";
+            lblApelido.Size = new Size(74, 25);
+            lblApelido.TabIndex = 0;
+            lblApelido.Text = "Apelido";
+            // 
+            // txtApelido
+            // 
+            txtApelido.Location = new Point(99, 33);
+            txtApelido.Name = "txtApelido";
+            txtApelido.Size = new Size(288, 31);
+            txtApelido.TabIndex = 1;
+            // 
+            // btnPesquisar
+            // 
+            btnPesquisar.Location = new Point(393, 33);
+            btnPesquisar.Name = "btnPesquisar";
+            btnPesquisar.Size = new Size(112, 34);
+            btnPesquisar.TabIndex = 2;
+            btnPesquisar.Text = "Pesquisar";
+            btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Apelido });
+            dataGridView1.Location = new Point(19, 70);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(486, 96);
+            dataGridView1.TabIndex = 3;
+            // 
+            // Apelido
+            // 
+            Apelido.HeaderText = "Apelido";
+            Apelido.MinimumWidth = 8;
+            Apelido.Name = "Apelido";
+            Apelido.Width = 150;
+            // 
+            // btnEnviarSolicitacao
+            // 
+            btnEnviarSolicitacao.Location = new Point(19, 172);
+            btnEnviarSolicitacao.Name = "btnEnviarSolicitacao";
+            btnEnviarSolicitacao.Size = new Size(163, 34);
+            btnEnviarSolicitacao.TabIndex = 4;
+            btnEnviarSolicitacao.Text = "Enviar solicitação";
+            btnEnviarSolicitacao.UseVisualStyleBackColor = true;
+            // 
+            // frmPesquisarUsuarios
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEnviarSolicitacao);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnPesquisar);
+            Controls.Add(txtApelido);
+            Controls.Add(lblApelido);
+            Name = "frmPesquisarUsuarios";
+            Text = "MSIF - Pesquisar usuários";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblApelido;
+        private TextBox txtApelido;
+        private Button btnPesquisar;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Apelido;
+        private Button btnEnviarSolicitacao;
+    }
+}
