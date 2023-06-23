1585093f0f583e3832212c7565fb963129c2d15e
gpurplem <megabrielbm@gmail.com>
2023-06-23 14:29:19 -0300

    Versão final

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,7 +8,8 @@ namespace MSIF.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Initial Catalog=DBMSIF; Integrated Security=SSPI; TrustServerCertificate=True");
+            //optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Initial Catalog=DBMSIF; Integrated Security=SSPI; TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=DBMSIF; User id=sa; Password=aaBB11.c; TrustServerCertificate=True");
         }
 
         public DbSet<Contatos> Contatos { get; set; }
